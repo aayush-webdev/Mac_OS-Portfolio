d73fc6352e13e2a03cc88d133ebfd9336e3eabab
Aayush Sharma <your-email@example.com>
2025-11-30 02:41:08 +0530

    update the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from '#components/navbar'
+import { Navbar, Welcome } from '#components/index'
 import React from 'react'
 
 const App = () => {
   return (
    <main>
     <Navbar />
+    <Welcome />
    </main>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from '#components/navbar'
+import Welcome from '#components/welcome'
+
+
+export{ Navbar, Welcome };
