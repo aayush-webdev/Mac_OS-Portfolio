de88e1dfd12b5b47c39afed243d3cba6229b5cc2
Aayush Sharma <your-email@example.com>
2025-12-05 14:26:51 +0530

    Fix import statement casing for Navbar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
-import Navbar from '#components/navbar'
+import Navbar from '#components/Navbar'
 import Welcome from '#components/Welcome'
 import Dock from '#components/dock'
 import WindowControls from '#components/WindowControls'
 import Home from '#components/Home'
 
-
-export{ Navbar, Welcome, Dock, WindowControls, Home };
+export { Navbar, Welcome, Dock, WindowControls, Home };
